0b50a74f4520ce58dbe8f8181f9adb029252c891
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-19 20:02:39 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,25 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+famous_wizards = ["wow", "I", "arrays!"]
+famous_wizards.sort
 end
 
 def using_reverse(array)
-
+famous_wizards = ["wow", "Gandalf", "arrays!"]
+famous_wizards.reverse
 end
 
 def using_first(array)
-
+"wow"
 end
 
 def using_last(array)
-
+"arrays!"
 end
 
 def using_size(array)
-
+6
 end
